98dec1a84a67d8c5aa6f04b4dc14775aac3ba953
Makkeru <makkeru@pop-os.localdomain>
2021-11-08 14:33:20 +0200

    teht 2.2: Riippuvuuksien hyodyntaminen

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,24 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        # apuprintti: print(content)
+
+        parsed_toml = toml.loads(content)
+        # apuprintti: print(parsed_toml)
+
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(
+            parsed_toml['tool']['poetry']['name'], 
+            parsed_toml['tool']['poetry']['description'], 
+            parsed_toml['tool']['poetry']['dependencies'], 
+            parsed_toml['tool']['poetry']['dev-dependencies'])
