db751d2e4deaf5fa2b5c99205ad30df8dd82aed1
Maikki Heijala <maikki.heijala@live.com>
2021-11-29 10:19:11 +0200

    teht 4.6: refaktoroitu intJoukon konstruktori ja tulostus

---
+++ viikko4/int-joukko/src/int_joukko.py
@@ -0,0 +1,130 @@
+KAPASITEETTI = 5
+OLETUSKASVATUS = 5
+
+
+class IntJoukko:
+    # siistitty konstruktori
+    def __init__(self, kapasiteetti=KAPASITEETTI, kasvatuskoko=OLETUSKASVATUS):
+        self.kapasiteetti = kapasiteetti
+        self.kasvatuskoko = kasvatuskoko
+        self.ljono = [0] * self.kapasiteetti
+        self.alkioiden_lkm = 0
+
+    def kuuluu(self, n):
+        on = 0
+
+        for i in range(0, self.alkioiden_lkm):
+            if n == self.ljono[i]:
+                on = on + 1
+
+        if on > 0:
+            return True
+        else:
+            return False
+
+    def lisaa(self, n):
+        ei_ole = 0
+
+        if self.alkioiden_lkm == 0:
+            self.ljono[0] = n
+            self.alkioiden_lkm = self.alkioiden_lkm + 1
+            return True
+        else:
+            pass
+
+        if not self.kuuluu(n):
+            self.ljono[self.alkioiden_lkm] = n
+            self.alkioiden_lkm = self.alkioiden_lkm + 1
+
+            if self.alkioiden_lkm % len(self.ljono) == 0:
+                taulukko_old = self.ljono
+                self.kopioi_taulukko(self.ljono, taulukko_old)
+                self.ljono = [0] * (self.alkioiden_lkm + self.kasvatuskoko)
+                self.kopioi_taulukko(taulukko_old, self.ljono)
+
+            return True
+
+        return False
+
+    def poista(self, n):
+        kohta = -1
+        apu = 0
+
+        for i in range(0, self.alkioiden_lkm):
+            if n == self.ljono[i]:
+                kohta = i  # siis luku löytyy tuosta kohdasta :D
+                self.ljono[kohta] = 0
+                break
+
+        if kohta != -1:
+            for j in range(kohta, self.alkioiden_lkm - 1):
+                apu = self.ljono[j]
+                self.ljono[j] = self.ljono[j + 1]
+                self.ljono[j + 1] = apu
+
+            self.alkioiden_lkm = self.alkioiden_lkm - 1
+            return True
+
+        return False
+
+    def kopioi_taulukko(self, a, b):
+        for i in range(0, len(a)):
+            b[i] = a[i]
+
+    def mahtavuus(self):
+        return self.alkioiden_lkm
+
+    def to_int_list(self):
+        taulu = [0] * self.alkioiden_lkm
+
+        for i in range(0, len(taulu)):
+            taulu[i] = self.ljono[i]
+
+        return taulu
+
+    @staticmethod
+    def yhdiste(a, b):
+        x = IntJoukko()
+        a_taulu = a.to_int_list()
+        b_taulu = b.to_int_list()
+
+        for i in range(0, len(a_taulu)):
+            x.lisaa(a_taulu[i])
+
+        for i in range(0, len(b_taulu)):
+            x.lisaa(b_taulu[i])
+
+        return x
+
+    @staticmethod
+    def leikkaus(a, b):
+        y = IntJoukko()
+        a_taulu = a.to_int_list()
+        b_taulu = b.to_int_list()
+
+        for i in range(0, len(a_taulu)):
+            for j in range(0, len(b_taulu)):
+                if a_taulu[i] == b_taulu[j]:
+                    y.lisaa(b_taulu[j])
+
+        return y
+
+    @staticmethod
+    def erotus(a, b):
+        z = IntJoukko()
+        a_taulu = a.to_int_list()
+        b_taulu = b.to_int_list()
+
+        for i in range(0, len(a_taulu)):
+            z.lisaa(a_taulu[i])
+
+        for i in range(0, len(b_taulu)):
+            z.poista(b_taulu[i])
+
+        return z
+
+    # siistitty tulostusmetodi
+    def __str__(self):
+        lukulista = self.ljono[:self.alkioiden_lkm]
+        palautettava = ', '.join(str(i) for i in lukulista)
+        return f'{{{palautettava}}}'
